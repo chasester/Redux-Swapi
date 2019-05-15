028b77e15f82f67011e25fa040192ddc29cf3e59
Chase Wex <clapingmanly@hotmail.com>
2019-05-15 17:54:34 -0400

    Fixed Character so spec

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { getCharacters} from "../actions/index"
 // import actions
 
 class CharacterListView extends React.Component {
@@ -11,13 +12,20 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
-    if (this.props.fetching) {
+    if (this.props.isfetching) {
       // return something here to indicate that you are fetching data
+      return <div style={{background: "#22222222", padding: "20px"}}>Loading....</div>
     }
-    return (
+    else if(this.props.error !== "")
+    {
+      console.log(this.props.error);
+      return <div style={{background: "red", padding: "20px"}}>{this.props.error.message}</div>
+    }
+    return ( 
       <div className="CharactersList_wrapper">
         <CharacterList characters={this.props.characters} />
       </div>
@@ -25,11 +33,21 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state =>
+{
+  return {
+    characters: state.charsReducer.characters,
+    isfetching: state.charsReducer.isfetching,
+    error: state.charsReducer.error
+  }
+}
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
